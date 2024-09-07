748c03cab30af362028634aac98b921c3cec5c5a
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2024-09-06 21:21:03 -0300

    Agregando JTextField para poder ingresar textos mediante un text box.

--- AppEscritorioPractica/src/Ejemplo2/Ventana.java
+++ AppEscritorioPractica/src/Ejemplo2/Ventana.java
@@ -5,25 +5,33 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	
 	private JButton boton;
+	private JTextField txtTexto;
 	
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500,500);
 		setLocation(350,350);
 		setTitle("AppEscritorio2");
+
+		txtTexto = new JTextField();
+		txtTexto.setColumns(10);
+		txtTexto.setBounds(63,33,200,36);
 		
 		boton = new JButton();
 		boton.setText("Aceptar");
 		boton.setBounds(10,15,140,50);
-		boton.addActionListener(new eventoBoton());
+		boton.addActionListener(new eventoBoton(txtTexto));
+		
 		
 		getContentPane().add(boton);
+		getContentPane().add(txtTexto);
 		
 		setLayout(null);
 	}
@@ -36,9 +44,14 @@ public class Ventana extends JFrame{
 
 class eventoBoton implements ActionListener{
 
+	private JTextField txtTexto;
+	
+	public eventoBoton(JTextField txt){
+		txtTexto = txt;
+	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		System.out.println("Hiciste click");
+		System.out.println("Hola, " + txtTexto.getText());
 		
 	}
 	

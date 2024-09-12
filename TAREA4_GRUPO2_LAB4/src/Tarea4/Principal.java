b6c16de8433a9ef3e77a55ad59d0c5144cb0a843
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2024-09-12 16:37:30 -0300

    Empezando TP4 App De Escritorio

---
+++ TAREA4_GRUPO2_LAB4/src/Tarea4/Principal.java
@@ -0,0 +1,61 @@
+package Tarea4;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGrupoNro = new JLabel("Grupo Nro: 2");
+		lblGrupoNro.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblGrupoNro.setBounds(15, 33, 154, 29);
+		contentPane.add(lblGrupoNro);
+		
+		JButton btnEjercicio = new JButton("Ejercicio 1");
+		btnEjercicio.setBounds(145, 78, 115, 29);
+		contentPane.add(btnEjercicio);
+		
+		JButton btnEjercicio_1 = new JButton("Ejercicio 2");
+		btnEjercicio_1.setBounds(145, 123, 115, 29);
+		contentPane.add(btnEjercicio_1);
+		
+		JButton btnEjercicio_2 = new JButton("Ejercicio 3");
+		btnEjercicio_2.setBounds(145, 168, 115, 29);
+		contentPane.add(btnEjercicio_2);
+	}
+}
